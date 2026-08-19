224319b15ff45600f6a56126a5630718b3e40732
alejagoguti-cpu <2.8531602e+08+alejagoguti-cpu@users.noreply.github.com>
2026-08-19 23:31:34 +0000

    feat: add skip link dark mode and action tooltips

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -86,7 +86,7 @@ function App() {
       <AccessibilityLayer />
       <ThemeProvider
         defaultTheme="light"
-        // switchable
+        switchable
       >
         <TooltipProvider>
           <Router />
